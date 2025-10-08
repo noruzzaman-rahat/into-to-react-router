fef1b66f7b4688c741f62474d7d0692c151d0eba
MD. Noruzzaman <tonmoyakand8@gmail.com>
2025-10-08 20:56:53 +0600

    third commit

---
+++ src/components/UserDetails/UserDetails.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useLoaderData, useParams } from 'react-router';
+
+const UserDetails = () => {
+    const user = useLoaderData();
+
+    const {userId} = useParams();
+    console.log(userId);
+
+    // const params = useParams();
+    // console.log(params);
+
+    // console.log(user);
+    const {website, name} = user;
+    return (
+        <div>
+            <h3>User Details here</h3>
+            <h5>Name: {name}</h5>
+            <p>Website: {website}</p>
+        </div>
+    );
+};
+
+export default UserDetails;
--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,9 @@ import Mobiles from './components/Mobiles/Mobiles.jsx';
 import Laptops from './components/Laptops/Laptops.jsx';
 import Users from './components/Users/Users.jsx';
 import Users2 from './components/Users2/Users2.jsx';
+import UserDetails from './components/UserDetails/UserDetails.jsx';
+import Posts from './components/Posts/Posts.jsx';
+import PostDetail from './components/PostDetail/PostDetail.jsx';
 
 
 const usersPromise = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
@@ -33,6 +36,27 @@ const router = createBrowserRouter([
         element: <Suspense fallback={<span>Loading......</span>}>
           <Users2 usersPromise={usersPromise} ></Users2>
         </Suspense>
+      },
+      {
+        path: 'user/:userId',
+        loader: ({params}) =>  fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`),
+
+         Component: UserDetails
+        // {
+        //   // console.log('params inside the loader', params.userId);
+         
+        // },
+       
+      },
+      {
+        path: 'posts',
+        loader: () => fetch('https://jsonplaceholder.typicode.com/posts'),
+        Component: Posts
+      },
+      {
+        path: 'posts/:postId',
+        loader: ({params}) => fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`),
+        Component: PostDetail
       }
     ]
   },
